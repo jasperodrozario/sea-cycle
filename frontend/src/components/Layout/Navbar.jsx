1c3b41a6a965dd6c680d9c5b68ad74dbea12ba9b
Jasper D Rozario <jasper.odrozario@gmail.com>
2025-08-12 21:04:59 +0600

    Add dashboard page

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full bg-opacity-80 text-white p-4 z-50">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-white-400">🌊 Sea-Cycle</h1>
+        <Link href={"/"}>
+          <h1 className="text-2xl font-bold text-white-400">🌊 Sea-Cycle</h1>
+        </Link>
         <div className="hidden md:flex space-x-6 items-center">
           <a href="#features" className="hover:text-cyan-400 transition-colors">
             Features
@@ -16,9 +19,11 @@ const Navbar = () => {
           <a href="#" className="hover:text-cyan-400 transition-colors">
             About Us
           </a>
-          <Button className="bg-white font-semibold text-black hover:bg-cyan-400 hover:text-white">
-            Go to Dashboard
-          </Button>
+          <Link href={"/dashboard"}>
+            <Button className="bg-white font-semibold text-black hover:bg-cyan-400 hover:text-white">
+              Go to Dashboard
+            </Button>
+          </Link>
         </div>
       </div>
     </nav>
