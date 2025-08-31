fff011f7ce96b9c1112b591edab66c897e145cee
Jasper D Rozario <jasper.odrozario@gmail.com>
2025-09-01 01:05:03 +0600

    Add missions page

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,21 +1,33 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-module.exports = function (req, res, next) {
-  // Get token from the header
-  const token = req.header("x-auth-token");
-
-  // Check if no token
-  if (!token) {
+// Middleware for authenticating non-admin users
+const auth = function (req, res, next) {
+  const authHeader = req.header("Authorization");
+  if (!authHeader)
     return res.status(401).json({ message: "No token, authorization denied." });
-  }
 
-  // Verify token
+  const token = authHeader.split(" ")[1];
+  if (!token)
+    return res.status(401).json({ message: "Token format is invalid." });
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user; // Add user payload to the request object
-    next(); // Move on to the next middleware or the route handler
+    req.user = decoded.user;
+    next();
   } catch (err) {
     res.status(401).json({ message: "Token is not valid." });
   }
 };
+
+// Middleware for authenticating Admins
+const admin = function (req, res, next) {
+  if (req.user.role !== "Admin") {
+    return res
+      .status(403)
+      .json({ message: "Access denied. Requires Admin role." });
+  }
+  next(); // If the user is an Admin, proceed
+};
+
+module.exports = { auth, admin };
